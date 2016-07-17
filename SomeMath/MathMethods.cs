daa6980d662e5518b4ab8348cbe47adf817eaa07
Dmitry Ryzhykh <ryzhykh.dmitry@gmail.com>
2016-07-17 15:37:18 +0300

    Refactoring. Delegates added. Only 206 LOC vs 295 before.

--- SomeMath/MathMethods.cs
+++ SomeMath/MathMethods.cs
@@ -37,15 +37,8 @@ namespace SomeMath
         /// <param name="secondNumber">Second number.</param>
         /// <param name="time">Elapsed time.</param>
         /// <returns>Greatest common divisor (GCD) of two numbers and elapsed time.</returns>
-        public static int EuclidAlgorithm(int firstNumber, int secondNumber, out long time)
-        {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            firstNumber = EuclidAlgorithm(firstNumber, secondNumber);
-            stopWatch.Stop();
-            time = stopWatch.ElapsedTicks;
-            return firstNumber;
-        }
+        public static int EuclidAlgorithm(int firstNumber, int secondNumber, out long time) =>
+            TimeSpent(EuclidAlgorithm, firstNumber, secondNumber, out time);        
 
         /// <summary>
         /// Method for computing the greatest common divisor (GCD) of numbers.
@@ -53,40 +46,15 @@ namespace SomeMath
         /// <param name="time">Elapsed time</param>
         /// <param name="numbers">Numbers</param>
         /// <returns>Greatest common divisor (GCD) of numbers and elapsed time.</returns>
-        public static int EuclidAlgorithm(out long time, params int[] numbers)
-        {
-            if (numbers == null) throw new NullReferenceException();
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            int result = EuclidAlgorithm(numbers);
-            stopWatch.Stop();
-            time = stopWatch.ElapsedTicks;
-            return result;
-        }
+        public static int EuclidAlgorithm(out long time, params int[] numbers) =>
+            TimeSpent(EuclidAlgorithm, numbers, out time);       
 
         /// <summary>
         /// Method for computing the greatest common divisor (GCD) of numbers.
         /// </summary>
         /// <param name="numbers">Numbers</param>
         /// <returns>Greatest common divisor (GCD) of numbers.</returns>
-        public static int EuclidAlgorithm(params int[] numbers)
-        {
-            if (numbers == null) throw new NullReferenceException();
-            int firstNumber = numbers[0];
-            int secondNumber;
-            for (int i = 1; i < numbers.Length; i++)
-            {
-                secondNumber = numbers[i];
-                int temp = 0;
-                while (secondNumber != 0)
-                {
-                    temp = secondNumber;
-                    secondNumber = firstNumber % secondNumber;
-                    firstNumber = temp;
-                }
-            }
-            return firstNumber;
-        }
+        public static int EuclidAlgorithm(params int[] numbers) => UniversalGCD(EuclidAlgorithm, numbers);      
         #endregion
 
         #region BinaryEuclideanAlgorithm
@@ -144,111 +112,15 @@ namespace SomeMath
         /// <param name="secondNumber">Second number.</param>
         /// <param name="time">Elapsed time.</param>
         /// <returns>Greatest common divisor (GCD) of two numbers and elapsed time.</returns>
-        public static int BinaryEuclidAlgorithm(int firstNumber, int secondNumber, out long time)
-        {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            int result = BinaryEuclidAlgorithm(firstNumber, secondNumber);
-            stopWatch.Stop();
-            time = stopWatch.ElapsedTicks;
-            return result;
-        }
+        public static int BinaryEuclidAlgorithm(int firstNumber, int secondNumber, out long time) =>
+            TimeSpent(BinaryEuclidAlgorithm, firstNumber, secondNumber, out time);        
 
         /// <summary>
         /// Method for computing the greatest common divisor (GCD) of numbers.
         /// </summary>
         /// <param name="numbers">Numbers.</param>
         /// <returns>Greatest common divisor (GCD) of numbers.</returns>
-        public static int BinaryEuclidAlgorithm(params int[] numbers)
-        {
-            if (numbers == null) throw new NullReferenceException();
-            int firstNumber = numbers[0];
-            int secondNumber = numbers[1];
-            int current = 2;
-            int power = 1;
-            while (true)
-            {
-                if (firstNumber == 0)
-                {
-                    if (current == numbers.Length)
-                    {
-                        secondNumber *= power;
-                        power = 1;
-                        return secondNumber;
-                    }
-                    firstNumber = numbers[current];
-                    current++;
-                    continue;
-                }
-                if (secondNumber == 0)
-                {
-                    if (current == numbers.Length)
-                    {
-                        firstNumber *= power;
-                        power = 1;
-                        return firstNumber;
-                    }
-                    secondNumber = numbers[current];
-                    current++;
-                    continue;
-                }
-                if (firstNumber == secondNumber)
-                {
-                    if (current == numbers.Length)
-                    {
-                        firstNumber *= power;
-                        power = 1;
-                        return firstNumber;
-                    }
-                    secondNumber = numbers[current];
-                    current++;
-                    continue;
-                }
-                if (firstNumber == 1 || secondNumber == 1)
-                {
-                    if (current == numbers.Length)
-                    {
-                        int temp = 1 * power;
-                        power = 1;
-                        return temp;
-                    }
-                    firstNumber = 1 * power;
-                    power = 1;
-                    secondNumber = numbers[current];
-                    current++;
-                    continue;
-                }
-                if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0))
-                {
-                    power *= 2;
-                    firstNumber = firstNumber >> 1;
-                    secondNumber = secondNumber >> 1;
-                    continue;
-                }
-                if ((firstNumber % 2 == 0) && (secondNumber % 2 != 0))
-                {
-                    firstNumber = firstNumber >> 1;
-                    continue;
-                }
-                if ((firstNumber % 2 != 0) && (secondNumber % 2 == 0))
-                {
-                    secondNumber = secondNumber >> 1;
-                    continue;
-                }
-                if (firstNumber % 2 != 0 && secondNumber % 2 != 0 && firstNumber > secondNumber)
-                {
-                    firstNumber = (firstNumber - secondNumber) >> 1;
-                    continue;
-                }
-                if (firstNumber % 2 != 0 && secondNumber % 2 != 0 && firstNumber < secondNumber)
-                {
-                    int temp = firstNumber;
-                    firstNumber = (secondNumber - firstNumber) >> 1;
-                    secondNumber = temp;
-                    continue;
-                }
-            }
-        }
+        public static int BinaryEuclidAlgorithm(params int[] numbers) => UniversalGCD(BinaryEuclidAlgorithm, numbers);        
 
         /// <summary>
         /// Method for computing the greatest common divisor (GCD) of numbers and elapsed time.
@@ -256,16 +128,9 @@ namespace SomeMath
         /// <param name="time">Elapsed time.</param>
         /// <param name="numbers">Numebrs.</param>
         /// <returns>Greatest common divisor (GCD) of numbers and elapsed time.</returns>
-        public static int BinaryEuclidAlgorithm(out long time, params int[] numbers)
-        {
-            if (numbers == null) throw new NullReferenceException();
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            int result = BinaryEuclidAlgorithm(numbers);
-            stopWatch.Stop();
-            time = stopWatch.ElapsedTicks;
-            return result;
-        }
+        public static int BinaryEuclidAlgorithm(out long time, params int[] numbers) =>
+            TimeSpent(BinaryEuclidAlgorithm, numbers, out time);
+       
         #endregion
 
         #region Newton method        
@@ -288,7 +153,54 @@ namespace SomeMath
 
                 } while (Math.Abs(x - result) > accuracy);
                 return result;
-            }        
+            }
+        #endregion
+
+        #region Helpers
+        /// <summary>
+        /// Universal method for all GCD.
+        /// </summary>
+        /// <param name="method">Method for GCD calculation.</param>
+        /// <param name="arg">Arguments.</param>
+        /// <returns>Int GCD.</returns>
+        private static int UniversalGCD(Func<int, int, int> method, int[] arg)
+        {
+            if (arg.Length == 0) throw new ArgumentNullException();
+            if (arg.Length == 1) return arg[0];
+            int temp = arg[0];
+            foreach (var item in arg)
+            {
+                temp = method(temp, item);
+            }
+            return temp;
+        }
+
+        /// <summary>
+        /// Calculating time spent for 2 arguments.
+        /// </summary>
+        /// <param name="method">Method.</param>
+        /// <param name="a">Argument 1.</param>
+        /// <param name="b">Argument 2.</param>
+        /// <param name="time">Time spent.</param>
+        /// <returns>GCD and time spent as out parameter.</returns>
+        private static int TimeSpent(Func<int, int, int> method, int a, int b, out long time) =>
+            TimeSpent((int[] x) => method(x[0], x[1]), new int[2] { a, b }, out time);
+
+        /// <summary>
+        /// Calculating time spent for arguments.
+        /// </summary>
+        /// <param name="method">Method.</param>
+        /// <param name="arg">Arguments.</param>
+        /// <param name="time">Time spent.</param>
+        /// <returns>GCD and time spent as out parameter.</returns>
+        private static int TimeSpent(Func<int[], int> method, int[] arg, out long time)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var res = method(arg);
+            watch.Stop();
+            time = watch.ElapsedTicks;
+            return res;
+        } 
         #endregion
     }
 }
